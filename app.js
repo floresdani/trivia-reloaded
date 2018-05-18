f1b9b25fdd3307d1aa49e04a90920b42218fae0f
Cote <majovardon@gmail.com>
2018-05-18 12:34:08 -0400

    las primeras preguntas y respuestas

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-
     function recibir(){ 
     var valor = document.getElementById("texto").value;
+    
         document.getElementById("txt").innerHTML="Bienvenid@ " + valor + ".";  
     } /* Ingresar el nombre */
-    
+    function studentQuestions(){
+        var questions = [
+            "¿Hay sede en Concepción?", 
+            "¿Hay egresados hombres?", 
+            ];
+        var respuestas = [
+            ["No", "Sí"],
+            ["No", "Sí"],
+        ]; 
+        document.getElementById("question").innerHTML = questions;
+    }
+    function placeQuestions(){
+
+    }
