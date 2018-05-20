e980c0707e38bd9f9d94788e1ee0eb98b24a7e82
Cote <majovardon@gmail.com>
2018-05-20 17:49:32 -0400

    Volví a la forma antigua porque la nueva no funcionaba

--- app.js
+++ app.js
@@ -3,32 +3,23 @@ function recibir(){
     
         document.getElementById("txt").innerHTML="Bienvenid@ " + valor + " ¿Quiéres jugar?.";  
 }   /* Ingresar el nombre */
+    /* Aceptar juego */
+function accept(){
     
-/* Aceptar juego */
-(function (){
-    var accept = document.getElementById("accept");
-    accept.addEventListener("click", function (){
-    document.getElementById("playing").innerHTML = question1;
-    var question1 = document.write("¿Han egresado hombres de Laboratoria? s/n");
-    if (question1() === "n") {
-        document.getElementById("rightAnswersText").innerHTML =
-            document.getElementById("rightAnswersText").innerHTML +
-            "<div class='answer'>" +
-            "<p>No han egresado hombres</p>"
-        "</div>";
-    } else {
-        document.getElementById("wrongAnswersText").innerHTML =
-            document.getElementById("wrongAnswersText").innerHTML +
-            "<div class='answer'>" +
-            "<p>No han egresado hombres</p>"
-        "</div>";
-    }});
-    });
-    
+}
     /* No aceptar juego */
-(function (){ 
-    var notAccept= document.getElementById("notAccept");
-    notAccept.addEventListener("click", function(){
-    document.getElementById("notPlaying").innerHTML = "Hasta pronto.";
-    });
-})
+function notAccept() {
+        document.getElementById("notPlaying").innerHTML = "Hasta pronto.";
+}
+function studentQuestions(){
+        var questions = [
+            "¿Hay sede en Concepción?", 
+            "¿Hay egresados hombres?", 
+            ];
+        var answers = [
+            ["No", "Sí"],
+            ["No", "Sí"],
+        ]; 
+        document.getElementById("question").innerHTML = questions;
+        document.getElementById("answer").innerHTML = answers;    
+    }
